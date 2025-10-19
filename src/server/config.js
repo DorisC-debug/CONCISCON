43fcf6d2ff3a8378a777d0e4434719a1e5171b2a
DorisC-debug <castrojimenezd93@gmail.com>
2025-10-19 15:22:41 -0400

    Update config.js

--- src/server/config.js
+++ src/server/config.js
@@ -1,12 +1,1 @@
 
-// config.js
-export const dbSettings = {
-  user: 'usuario1',
-  password: 'D@ris27022006',
-  server: 'smartgrade.database.windows.net',
-  database: 'BacpacCIS1',
-  options: {
-    encrypt: true,
-    trustServerCertificate: false
-  }
-};
